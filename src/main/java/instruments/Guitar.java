91aaccf3fa73e2209c179da4d4973baca7d412c9
Grant Houston <grant.houston44@hotmail.com>
2020-02-07 15:28:24 +0000

    Guitar Class

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,4 +1,14 @@
 package instruments;
 
-public class Guitar {
+public class Guitar extends Instrument{
+
+    public Guitar(String material, String colour, InstrumentType type, double buyPrice, double sellPrice){
+    super(material,colour,type,buyPrice,sellPrice);
+    }
+
+
+    public String play(){
+        return "A really good song on guitar is thunderstruck.....";
+    }
+
 }
