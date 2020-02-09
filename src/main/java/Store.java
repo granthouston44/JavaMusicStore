57df1e7165c7587882a20034156f490157d1ce45
Grant Houston <grant.houston44@hotmail.com>
2020-02-09 16:14:38 +0000

    can add to stock

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,2 +1,32 @@
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Store {
+
+    private ArrayList<ISell> stock;
+    private double till;
+
+    public Store(){
+        this.stock = new ArrayList<ISell>();
+        this.till = 0;
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void addToStock(ISell stockITem){
+        stock.add(stockITem);
+    }
+
+    public void removeFromStock(ISell stockITem){
+        stock.remove(stockITem);
+    }
+
+
 }
---
+++ src/test/java/StoreTest.java
@@ -0,0 +1,39 @@
+import accessories.GuitarStrings;
+import instruments.Drums;
+import instruments.Guitar;
+import instruments.InstrumentType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StoreTest {
+
+    private Store store;
+    private Drums drums;
+    private Guitar guitar;
+    private GuitarStrings strings;
+
+
+    @Before
+    public void before(){
+        store = new Store();
+        drums = new Drums("drum skin", "blue", InstrumentType.PERCUSSION, 200, 350, "junior kit");
+        guitar = new Guitar("mahogony", "black",InstrumentType.STRING, 300, 425);
+        strings = new GuitarStrings("Ernie balls", 5, 20, "good set of strings");
+    }
+
+    @Test
+    public void canAddToStock(){
+        store.addToStock(drums);
+        store.addToStock(guitar);
+        store.addToStock(strings);
+        assertEquals(3, store.getStock().size());
+    }
+
+    @Test
+    public void canRemoveFromStock(){
+
+    }
+
+}
